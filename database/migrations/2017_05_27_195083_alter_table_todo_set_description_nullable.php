4c04d6cdde15df25f5087601a8f23ad8484ae9ef
TZK- <thib-17@orange.fr>
2017-05-27 19:08:14 +0200

    Do not copy paste file without changing classname :p

--- database/migrations/2017_05_27_195083_alter_table_todo_set_description_nullable.php
+++ database/migrations/2017_05_27_195083_alter_table_todo_set_description_nullable.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterTableTodoAddUserId extends Migration
+class AlterTableTodoSetDescriptionNullable extends Migration
 {
     /**
      * Run the migrations.
