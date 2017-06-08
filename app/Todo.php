3dc89ac8807645edd091762946ec4f41066d4241
GwenaelCruz <gwenael.cruz@iut-valence.fr>
2017-06-08 09:07:10 +0000

    Added ended to fillable

--- app/Todo.php
+++ app/Todo.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     protected $fillable = [
-        'title', 'description',
+        'title', 'description', 'ended'
     ];
 
     public function user()
